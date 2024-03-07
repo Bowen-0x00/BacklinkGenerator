611ed492bfa89d9427861ea891f6e9f406feb86d
Bowen <BowenLiu0001@gmail.com>
2024-03-07 10:24:21 +0800

    Revise base64 string error.

--- src/utils/image.py
+++ src/utils/image.py
@@ -10,7 +10,7 @@ def image_to_base64(image):
             image.save(byte_stream, format="PNG")
             byte_stream.seek(0)
             encoded_string = base64.b64encode(byte_stream.read())
-            return f'data:image/png:base64,{encoded_string.decode("utf-8")}'
+            return f'data:image/png;base64,{encoded_string.decode("utf-8")}'
 # def image_to_base64(image):
 #     with BytesIO() as byte_stream:
 #         image.save(byte_stream, format="PNG")
